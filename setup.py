46196b8f79e3c00d7aef49fc220b5e4710b41c8c
Madis Peebo <madis@nevercode.io>
2019-11-21 09:55:33 +0200

    fix command name

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
             "storage = codemagic.storage:Storage.invoke_cli",
             "grab = codemagic.grab:Grab.invoke_cli",
             "keychain = codemagic.keychain:Keychain.invoke_cli",
-            "changelog = codemagic.git_changelog:GitChangelog.invoke_cli",
+            "git-changelog = codemagic.git_changelog:GitChangelog.invoke_cli",
             "universal-apk = codemagic.universal_apk_generator:UniversalApkGenerator.invoke_cli",
         ]
     },
