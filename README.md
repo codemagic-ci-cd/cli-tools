b0ef3a16f75a8be670507d42c4025a4c6309f51d
Madis Peebo <madis@nevercode.io>
2019-11-22 16:49:49 +0200

    rename packages, add twine, readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # cli-tools
+
+## Installing
+
+```
+pip3 install codemagic-cli-tools
+```
+
+## Packaging for distribution
+
+Don't forget the version number
+
+```
+python3 setup.py sdist bdist_wheel
+```
+
+## Publishing package (test)
+
+```
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+```
+
+## Publishing package (real)
+
+```
+python3 -m twine upload dist/*
+```
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_version():
 
 
 setup(
-    name='codemagic',
+    name='codemagic-cli-tools',
     version=get_version(),
     url='https://github.com/codemagic-ci-cd/cli-tools',
     project_urls={
@@ -29,11 +29,11 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "storage = codemagic.storage:Storage.invoke_cli",
-            "grab = codemagic.grab:Grab.invoke_cli",
-            "keychain = codemagic.keychain:Keychain.invoke_cli",
-            "git-changelog = codemagic.git_changelog:GitChangelog.invoke_cli",
-            "universal-apk = codemagic.universal_apk_generator:UniversalApkGenerator.invoke_cli",
+            "storage = codemagic_cli_tools.storage:Storage.invoke_cli",
+            "grab = codemagic_cli_tools.grab:Grab.invoke_cli",
+            "keychain = codemagic_cli_tools.keychain:Keychain.invoke_cli",
+            "git-changelog = codemagic_cli_tools.git_changelog:GitChangelog.invoke_cli",
+            "universal-apk = codemagic_cli_tools.universal_apk_generator:UniversalApkGenerator.invoke_cli",
         ]
     },
 )
