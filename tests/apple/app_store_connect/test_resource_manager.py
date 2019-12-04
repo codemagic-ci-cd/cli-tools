544e6fa8e9db2bc1306984be34f5b8e9587228fd
Priit Lätt <priit@nevercode.io>
2019-12-04 14:15:54 +0200

    Clean up tests

--- tests/apple/app_store_connect/test_resource_manager.py
+++ tests/apple/app_store_connect/test_resource_manager.py
@@ -1,37 +1,33 @@
 import enum
 from dataclasses import dataclass
-from datetime import datetime
-from datetime import timezone
 from typing import Optional
 
 import pytest
 
 from codemagic_cli_tools.apple.app_store_connect.resource_manager import ResourceManager
 
-NOW = datetime(year=2019, month=8, day=20)
-UTC_NOW = datetime(year=2019, month=8, day=20, tzinfo=timezone.utc)
+StubEnum = enum.Enum('StubEnum', {'A': 'a', 'B': 'b'})
 
 
-class StubEnum(enum.Enum):
-    A = 'a'
-    B = 'b'
+class CustomString(str):
+    ...
 
 
 @dataclass
 class StubFilter(ResourceManager.Filter):
     field_one: Optional[str] = None
     field_two: Optional[StubEnum] = None
-    field_three_four: Optional[str] = None
+    field_three_four: Optional[CustomString] = None
 
 
 @pytest.mark.parametrize('filter_params, expected_query_params', [
     ({'field_one': '1'}, {'filter[fieldOne]': '1'}),
-    ({'field_two': '2'}, {'filter[fieldTwo]': '2'}),
-    ({'field_three_four': '34'}, {'filter[fieldThreeFour]': '34'}),
+    ({'field_two': StubEnum.A}, {'filter[fieldTwo]': 'a'}),
+    ({'field_three_four': CustomString('34')}, {'filter[fieldThreeFour]': '34'}),
     ({'field_one': '1', 'field_two': None}, {'filter[fieldOne]': '1'}),
-    ({'field_two': '2', 'field_one': None}, {'filter[fieldTwo]': '2'}),
-    ({'field_three_four': '34', 'field_one': None, 'field_two': None}, {'filter[fieldThreeFour]': '34'}),
-    ({'field_one': '1', 'field_two': '2'}, {'filter[fieldOne]': '1', 'filter[fieldTwo]': '2'}),
+    ({'field_two': StubEnum.B, 'field_one': None}, {'filter[fieldTwo]': 'b'}),
+    ({'field_three_four': CustomString('34'), 'field_one': None, 'field_two': None}, {'filter[fieldThreeFour]': '34'}),
+    ({'field_one': '1', 'field_two': StubEnum.A}, {'filter[fieldOne]': '1', 'filter[fieldTwo]': 'a'}),
 ])
 def test_resource_manager_filter_to_params_conversion(filter_params, expected_query_params):
     test_filter = StubFilter(**filter_params)
