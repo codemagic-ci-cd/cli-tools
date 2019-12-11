699c34f187e17647ce9309c3b87868147f3222ef
Priit Lätt <priit@nevercode.io>
2019-12-11 11:25:16 +0200

    Add tests to string to bytes conversions

--- src/codemagic_cli_tools/models/byte_str_converter.py
+++ src/codemagic_cli_tools/models/byte_str_converter.py
@@ -6,13 +6,13 @@ class BytesStrConverter:
     @classmethod
     def _bytes(cls, str_or_bytes: AnyStr) -> bytes:
         if isinstance(str_or_bytes, str):
-            return str_or_bytes.encode()
+            return str_or_bytes.encode(encoding='utf-8')
         else:
             return str_or_bytes
 
     @classmethod
     def _str(cls, str_or_bytes: AnyStr) -> str:
         if isinstance(str_or_bytes, bytes):
-            return str_or_bytes.decode()
+            return str_or_bytes.decode(encoding='utf-8')
         else:
             return str_or_bytes
---
+++ tests/models/test_byte_str_converter.py
@@ -0,0 +1,33 @@
+import pytest
+
+from codemagic_cli_tools.models.byte_str_converter import BytesStrConverter
+
+
+@pytest.mark.parametrize('bytes_or_str, expected_result', [
+    ('', b''),
+    ('test', b'test'),
+    ('á', b'\xc3\xa1'),
+    ('Привет, мир', b'\xd0\x9f\xd1\x80\xd0\xb8\xd0\xb2\xd0\xb5\xd1\x82, \xd0\xbc\xd0\xb8\xd1\x80'),
+    (r'¯\_( ͡° ͜ʖ ͡°)_/¯', b'\xc2\xaf\\_( \xcd\xa1\xc2\xb0 \xcd\x9c\xca\x96 \xcd\xa1\xc2\xb0)_/\xc2\xaf'),
+    ('🤩', b'\xf0\x9f\xa4\xa9'),
+    (b'\xf0\x9f\xa4\xa9', b'\xf0\x9f\xa4\xa9'),
+    (b'', b''),
+    (b'test', b'test')
+])
+def test_to_bytes(bytes_or_str, expected_result):
+    assert BytesStrConverter._bytes(bytes_or_str) == expected_result
+
+
+@pytest.mark.parametrize('bytes_or_str, expected_result', [
+    ('', ''),
+    ('test', 'test'),
+    (b'\xc3\xa1', 'á'),
+    (b'\xd0\x9f\xd1\x80\xd0\xb8\xd0\xb2\xd0\xb5\xd1\x82, \xd0\xbc\xd0\xb8\xd1\x80', 'Привет, мир'),
+    (b'\xc2\xaf\\_( \xcd\xa1\xc2\xb0 \xcd\x9c\xca\x96 \xcd\xa1\xc2\xb0)_/\xc2\xaf', r'¯\_( ͡° ͜ʖ ͡°)_/¯'),
+    (b'\xf0\x9f\xa4\xa9', '🤩'),
+    ('🤩', '🤩'),
+    (b'', ''),
+    (b'test', 'test')
+])
+def test_to_str(bytes_or_str, expected_result):
+    assert BytesStrConverter._str(bytes_or_str) == expected_result
