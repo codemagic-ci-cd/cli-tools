bd105a8d655aa4aeb0aab192a7a8199e896a7368
Priit Lätt <lattpriit@gmail.com>
2020-06-02 11:30:19 +0300

    Add timeouts to reading from streams

--- src/codemagic/__version__.py
+++ src/codemagic/__version__.py
@@ -1,5 +1,5 @@
 __title__ = "codemagic-cli-tools"
 __description__ = "CLI tools used in Codemagic builds"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __url__ = 'https://github.com/codemagic-ci-cd/cli-tools'
 __licence__ = 'GNU General Public License v3.0'
