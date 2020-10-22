301d3d0446a65aeb0243cf0ccb747fe5ba40ea10
Priit Lätt <lattpriit@gmail.com>
2020-10-22 11:59:52 +0300

    Remove metaclass from RunningCliAppMixin

--- src/codemagic/mixins/running_cli_app.py
+++ src/codemagic/mixins/running_cli_app.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import abc
 from typing import Optional
 from typing import TYPE_CHECKING
 
@@ -8,11 +7,7 @@ if TYPE_CHECKING:
     from codemagic.cli import CliApp
 
 
-class RunningCliAppMixin(metaclass=abc.ABCMeta):
-
-    @classmethod
-    def cli_app(cls) -> Optional[CliApp]:
-        return cls.get_current_cli_app()
+class RunningCliAppMixin:
 
     @classmethod
     def get_current_cli_app(cls) -> Optional[CliApp]:
