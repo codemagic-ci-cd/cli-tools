1f7044baa0870a52940ad0393708f49e2764dbd9
Priit Lätt <lattpriit@gmail.com>
2020-10-22 12:06:20 +0300

    Skip not-implemented test

--- tests/models/xctests/test_xctest_models.py
+++ tests/models/xctests/test_xctest_models.py
@@ -1,8 +1,11 @@
 import json
 
+import pytest
+
 from codemagic.models.xctests.xctest_models import ActionsInvocationRecord
 
 
+@pytest.mark.skip(reason='Test is not ready')
 def test_models():
     test_result = json.load(open('/tmp/test-result.json'))
     air = ActionsInvocationRecord(test_result)
