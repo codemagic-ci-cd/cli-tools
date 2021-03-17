e168d967856e92eff8994f7f7b75f48c33c516aa
Priit Lätt <lattpriit@gmail.com>
2021-03-17 13:08:14 +0200

    Update error response model to respect error meta and associated errors

--- src/codemagic/apple/resources/error_response.py
+++ src/codemagic/apple/resources/error_response.py
@@ -1,12 +1,34 @@
 from __future__ import annotations
 
+import textwrap
 from dataclasses import dataclass
 from typing import Dict
+from typing import List
 from typing import Optional
 
 from requests import Response
 
 from .resource import DictSerializable
+
+
+@dataclass
+class ErrorMeta(DictSerializable):
+    associatedErrors: Optional[Dict[str, List[Error]]] = None
+
+    def __post_init__(self):
+        if self.associatedErrors is None:
+            return
+        for scope in self.associatedErrors.keys():
+            self.associatedErrors[scope] = [
+                Error(**error) if isinstance(error, dict) else error
+                for error in self.associatedErrors[scope]
+            ]
+
+    def __str__(self):
+        lines = []
+        for scope, errors in (self.associatedErrors or {}).items():
+            lines.extend(f'Associated error: {e}' for e in errors)
+        return '\n'.join(lines)
 
 
 @dataclass
@@ -17,6 +39,19 @@ class Error(DictSerializable):
     detail: str
     id: Optional[str] = None
     source: Optional[Dict[str, str]] = None
+    meta: Optional[ErrorMeta] = None
+
+    def __post_init__(self):
+        if isinstance(self.meta, dict):
+            self.meta = ErrorMeta(**self.meta)
+
+    def __str__(self):
+        s = f'{self.title} - {self.detail}'
+        if self.meta:
+            meta = textwrap.indent(str(self.meta), '\t')
+            if meta:
+                s += f'\n{meta}'
+        return s
 
 
 class ErrorResponse(DictSerializable):
@@ -36,4 +71,4 @@ class ErrorResponse(DictSerializable):
         return error_response
 
     def __str__(self):
-        return '\n'.join(f'{error.title} - {error.detail}' for error in self.errors)
+        return '\n'.join(map(str, self.errors))
