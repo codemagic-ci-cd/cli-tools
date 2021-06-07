392a72c9978bb2242512cea76a7629485d03fb0f
Artemii Yanushevskyi <artemii@nevercode.io>
2021-06-07 15:11:32 +0300

    No QA

--- tests/tools/app_store_connect/test_publish_action.py
+++ tests/tools/app_store_connect/test_publish_action.py
@@ -7,7 +7,7 @@ from unittest import mock
 
 import pytest
 
-from codemagic.tools._app_store_connect.arguments import PublishArgument
+from codemagic.tools._app_store_connect.arguments import PublishArgument  # noqa
 from codemagic.tools.app_store_connect import AppStoreConnect
 from codemagic.tools.app_store_connect import AppStoreConnectArgument
 from codemagic.tools.app_store_connect import Types
