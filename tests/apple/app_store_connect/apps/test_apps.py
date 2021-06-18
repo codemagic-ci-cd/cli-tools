cb2d7bacfb30390b6c8dfbc8c082238feb64bf76
Priit Lätt <lattpriit@gmail.com>
2021-06-18 13:33:00 +0300

    Simplify tests

--- tests/apple/app_store_connect/apps/test_apps.py
+++ tests/apple/app_store_connect/apps/test_apps.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 import pytest
@@ -27,18 +26,22 @@ class AppsTest(ResourceManagerTestsBase):
         assert app.id == app_id
         assert app.type is ResourceType.APPS
 
-    def test_get_app_infos(self):
+    def test_read_beta_app_localizations(self):
         banaan_app_id = ResourceId('1481211155')  # Banaan iOS
         capybara_app_id = ResourceId('1496105355')  # CapybaraApp
-        for name, app_id in zip(['Banaan', 'Capybara'], [banaan_app_id, capybara_app_id]):
+        for app_id in (banaan_app_id, capybara_app_id):
             beta_app_localizations = self.api_client.apps.list_beta_app_localizations(app_id)
-            beta_app_review_detail = self.api_client.apps.read_beta_app_review_detail(app_id)
-            print(80*'=')
-            print(name)
-            print(80*'=')
             for beta_app_localization in beta_app_localizations:
-                print(json.dumps(beta_app_localization._raw['attributes'], indent=4))
-            print(json.dumps(beta_app_review_detail._raw['attributes'], indent=4))
+                assert beta_app_localization.type is ResourceType.BETA_APP_LOCALIZATIONS
+                assert beta_app_localization.relationships.app
+
+    def test_read_beta_app_review_detail(self):
+        banaan_app_id = ResourceId('1481211155')  # Banaan iOS
+        capybara_app_id = ResourceId('1496105355')  # CapybaraApp
+        for app_id in (banaan_app_id, capybara_app_id):
+            beta_app_review_detail = self.api_client.apps.read_beta_app_review_detail(app_id)
+            assert beta_app_review_detail.type is ResourceType.BETA_APP_REVIEW_DETAILS
+            assert beta_app_review_detail.relationships.app
 
     def test_read_not_found(self):
         with pytest.raises(AppStoreConnectApiError) as exception_info:
