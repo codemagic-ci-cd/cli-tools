e7dfbc63c83fb8ba257f77f1c55082b651668efa
Priit Lätt <lattpriit@gmail.com>
2021-06-22 12:20:31 +0300

    Add tests for ResourceEnum graceful fallback

--- src/codemagic/google_play/resources/enums.py
+++ src/codemagic/google_play/resources/enums.py
@@ -1,19 +1,19 @@
 import enum
 
 
-class _ResourceEnum(enum.Enum):
+class ResourceEnum(enum.Enum):
     def __str__(self):
         return str(self.value)
 
 
-class TrackName(_ResourceEnum):
+class TrackName(ResourceEnum):
     INTERNAL = 'internal'
     ALPHA = 'alpha'
     BETA = 'beta'
     PRODUCTION = 'production'
 
 
-class ReleaseStatus(_ResourceEnum):
+class ReleaseStatus(ResourceEnum):
     STATUS_UNSPECIFIED = 'statusUnspecified'
     DRAFT = 'draft'
     IN_PROGRESS = 'inProgress'
