805f36185e3b793d43a0456406d0364c27d5a384
Artemii Yanushevskyi <artemii@nevercode.io>
2021-07-05 15:39:35 +0300

    Refactor

--- src/codemagic/tools/_app_store_connect/action_groups/beta_groups_action_group.py
+++ src/codemagic/tools/_app_store_connect/action_groups/beta_groups_action_group.py
@@ -23,13 +23,7 @@ class BetaGroupsActionGroup(AbstractBaseAction):
         """
         Add build to a Beta group
         """
-        app = self.api_client.builds.read_app(build_id)
-
-        resource_filter = self.api_client.beta_groups.Filter(app=app.id)
-        app_beta_groups = self.api_client.beta_groups.list(resource_filter=resource_filter)
-
-        matched_beta_groups = set(
-            beta_group for beta_group in app_beta_groups if beta_group.attributes.name in beta_group_names)
+        matched_beta_groups, matched_beta_group_names = self._get_beta_groups(build_id, beta_group_names)
 
         errors = []
         for beta_group in matched_beta_groups:
@@ -39,13 +33,11 @@ class BetaGroupsActionGroup(AbstractBaseAction):
             except AppStoreConnectApiError as e:
                 errors.append([beta_group_name, e.error_response])
             else:
-                self.logger.error(Colors.GREEN(f"Added build '{build_id}' to '{beta_group_name}' beta group"))
+                self.logger.info(Colors.GREEN(f"Added build '{build_id}' to '{beta_group_name}' beta group"))
 
-        missing_beta_group_names = \
-            set(beta_group_names) - set(beta_group.attributes.name for beta_group in matched_beta_groups)
-
+        missing_beta_group_names = set(beta_group_names) - matched_beta_group_names
         if missing_beta_group_names:
-            self.logger.info(Colors.YELLOW(
+            self.logger.warning(Colors.YELLOW(
                 '\n'.join(f"Cannot find Beta group with the name '{name}'" for name in missing_beta_group_names)))
 
         if errors:
@@ -64,13 +56,7 @@ class BetaGroupsActionGroup(AbstractBaseAction):
         """
         Remove build from a Beta group
         """
-        app = self.api_client.builds.read_app(build_id)
-
-        resource_filter = self.api_client.beta_groups.Filter(app=app.id)
-        app_beta_groups = self.api_client.beta_groups.list(resource_filter=resource_filter)
-
-        matched_beta_groups = set(
-            beta_group for beta_group in app_beta_groups if beta_group.attributes.name in beta_group_names)
+        matched_beta_groups, matched_beta_group_names = self._get_beta_groups(build_id, beta_group_names)
 
         errors = []
         for beta_group in matched_beta_groups:
@@ -80,13 +66,11 @@ class BetaGroupsActionGroup(AbstractBaseAction):
             except AppStoreConnectApiError as e:
                 errors.append([beta_group_name, e.error_response])
             else:
-                self.logger.error(Colors.GREEN(f"Removed build '{build_id}' from '{beta_group_name}' beta group"))
+                self.logger.info(Colors.GREEN(f"Removed build '{build_id}' from '{beta_group_name}' beta group"))
 
-        missing_beta_group_names = \
-            set(beta_group_names) - set(beta_group.attributes.name for beta_group in matched_beta_groups)
-
+        missing_beta_group_names = set(beta_group_names) - matched_beta_group_names
         if missing_beta_group_names:
-            self.logger.info(Colors.YELLOW(
+            self.logger.warning(Colors.YELLOW(
                 '\n'.join(f"Cannot find Beta group with the name '{name}'" for name in missing_beta_group_names)))
 
         if errors:
@@ -96,3 +80,16 @@ class BetaGroupsActionGroup(AbstractBaseAction):
                     message.format(name=name, error_response=error_response) for name, error_response in errors
                 ),
             )
+
+    def _get_beta_groups(self, build_id: Union[ResourceId, Build], beta_group_names: Sequence[str]):
+        app = self.api_client.builds.read_app(build_id)
+
+        resource_filter = self.api_client.beta_groups.Filter(app=app.id)
+        app_beta_groups = self.api_client.beta_groups.list(resource_filter=resource_filter)
+
+        matched_beta_groups = set(
+            beta_group for beta_group in app_beta_groups if beta_group.attributes.name in beta_group_names)
+
+        matched_beta_group_names = set(beta_group for beta_group in app_beta_groups)
+
+        return [matched_beta_groups, matched_beta_group_names]
