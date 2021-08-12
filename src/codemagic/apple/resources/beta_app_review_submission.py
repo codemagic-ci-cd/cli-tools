8687ca8d59bccb982fa6012cb1a6d1cec2aa7ccf
Artemii Yanushevskyi <artemii@nevercode.io>
2021-08-12 11:20:16 +0300

    Fix: missing submittedDate attribute

--- src/codemagic/apple/resources/beta_app_review_submission.py
+++ src/codemagic/apple/resources/beta_app_review_submission.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
 
 from .enums import BetaReviewState
 from .resource import Relationship
@@ -15,6 +17,7 @@ class BetaAppReviewSubmission(Resource):
     @dataclass
     class Attributes(Resource.Attributes):
         betaReviewState: BetaReviewState
+        submittedDate: Optional[datetime]
 
         def __post_init__(self):
             if isinstance(self.betaReviewState, str):
