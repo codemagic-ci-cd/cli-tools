54036795018840fa6b08cfe598b3464f8ae38fef
Priit Lätt <priit@nevercode.io>
2022-01-11 18:59:39 +0200

    Update src/codemagic/tools/_xcode_project/arguments.py
    Co-authored-by: Artemii <40743076+artemii-yanushevskyi@users.noreply.github.com>

--- src/codemagic/tools/_xcode_project/arguments.py
+++ src/codemagic/tools/_xcode_project/arguments.py
@@ -227,7 +227,7 @@ class TestArgument(cli.Argument):
             '"iPad Pro (9.7-inch)", '
             '"tvOS 14.1 Apple TV 4K (at 1080p)", '
             '"Apple TV 4K". '
-            'Default test destination will be chosen if no devices are specified and test SDK is not'
+            'Default test destination will be chosen if no devices are specified and test SDK is not '
             'targeting macOS. For macOS tests no destination are specified. '
             '(See `xcode-project default-test-destination` for more information about default destination).'
         ),
