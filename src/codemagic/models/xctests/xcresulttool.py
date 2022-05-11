c8a2ce88c916ac6b5f66f23ae92e0040c63e37c4
Priit Lätt <lattpriit@gmail.com>
2022-05-11 15:25:41 +0300

    Pipe xcresulttool output to file

--- src/codemagic/models/xctests/xcresulttool.py
+++ src/codemagic/models/xctests/xcresulttool.py
@@ -4,7 +4,9 @@ import json
 import pathlib
 import subprocess
 from tempfile import NamedTemporaryFile
+from typing import TYPE_CHECKING
 from typing import Any
+from typing import AnyStr
 from typing import Dict
 from typing import List
 from typing import Optional
@@ -12,6 +14,9 @@ from typing import Sequence
 
 from codemagic.cli import CommandArg
 from codemagic.mixins import RunningCliAppMixin
+
+if TYPE_CHECKING:
+    from codemagic.cli import CliApp
 
 
 class XcResultTool(RunningCliAppMixin):
@@ -44,14 +49,28 @@ class XcResultTool(RunningCliAppMixin):
         return output_path
 
     @classmethod
-    def _run_command(cls, command_args: Sequence[CommandArg], error_message: str) -> str:
+    def _run_command(cls, command_args: Sequence[CommandArg], error_message: str) -> AnyStr:
         cli_app = cls.get_current_cli_app()
         try:
             if cli_app:
-                process = cli_app.execute(command_args, suppress_output=True)
-                process.raise_for_returncode()
-                return process.stdout
+                return cls._run_command_with_cli_app(cli_app, command_args)
             else:
                 return subprocess.check_output(command_args).decode()
         except subprocess.CalledProcessError:
             raise IOError(error_message)
+
+    @classmethod
+    def _run_command_with_cli_app(cls, cli_app: CliApp, command_args: Sequence[CommandArg]) -> bytes:
+        """
+        Replace default stdout stream with direct file handle to bypass stream
+        processing in Python which can be very slow. For example
+        `xcrun xcresulttool get --format json --path results.xcresult --id 'object-id'`
+        can output 500K+ lines at 30+ MB. Processing it in small chunks in Python is very time
+        consuming whereas using file handles is almost instantaneous.
+        """
+        with NamedTemporaryFile(mode='w+b') as stdout_fd:
+            process = cli_app.execute(command_args, suppress_output=True, stdout=stdout_fd)
+            process.raise_for_returncode()
+            stdout_fd.flush()
+            stdout_fd.seek(0)
+            return stdout_fd.read()
