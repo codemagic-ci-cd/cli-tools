33314191ba3952ffe324c43823ce91308ce44f73
priitlatttest <lattpriit+test@gmail.com>
2022-05-17 11:50:41 +0300

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,14 @@
+import pytest
+
+
 def test_fail():
     assert False, "This shall not pass"
 
 
 def test_succeed():
     assert True, "This shall pass"
+
+
+@pytest.mark.skip(reason='Codesign not available')
+def test_skip():
+    pass # Skipped
