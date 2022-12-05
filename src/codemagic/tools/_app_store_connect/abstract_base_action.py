773e0454f48dee77432e4b80c3010f57c8ad0ab9
VeArnold <arnold.veltmann@gmail.com>
2022-12-05 19:26:10 +0700

    Sync abstractmethod

--- src/codemagic/tools/_app_store_connect/abstract_base_action.py
+++ src/codemagic/tools/_app_store_connect/abstract_base_action.py
@@ -214,7 +214,7 @@ class AbstractBaseAction(ResourceManagerMixin, PathFinderMixin, metaclass=ABCMet
     @abstractmethod
     def cancel_review_submissions(
         self,
-        application_id: Optional[ResourceId] = None,
+        application_id: ResourceId = None,
         platform: Optional[Platform] = None,
         review_submission_state: Optional[Union[ReviewSubmissionState, Sequence[ReviewSubmissionState]]] = None,
         should_print: bool = False,
