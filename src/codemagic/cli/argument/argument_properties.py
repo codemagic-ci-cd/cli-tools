d7cc4a21e31450829d372821817b8fb7d60f5625
Priit Lätt <priit@nevercode.io>
2022-12-20 16:06:21 +0200

    Bugfix: Python 3.11.1 compatibility (#295)

--- src/codemagic/cli/argument/argument_properties.py
+++ src/codemagic/cli/argument/argument_properties.py
@@ -20,7 +20,13 @@ class ArgumentProperties(NamedTuple):
     argparse_kwargs: Optional[Dict[str, Any]] = None
     argument_group_name: Optional[str] = None
 
-    def duplicate(self, **overwrites) -> ArgumentProperties:
+    @classmethod
+    def duplicate(cls, template: Union[Tuple, ArgumentProperties], **overwrites) -> ArgumentProperties:
+        if not isinstance(template, ArgumentProperties):
+            template = cls(*template)
+        return template._duplicate(**overwrites)
+
+    def _duplicate(self, **overwrites) -> ArgumentProperties:
         kwargs = {}
         for field in self._fields:
             if field in overwrites:
@@ -29,6 +35,14 @@ class ArgumentProperties(NamedTuple):
                 kwargs[field] = copy.deepcopy(getattr(self, field))
 
         return ArgumentProperties(**kwargs)
+
+    @classmethod
+    def get_flag(cls, argument_properties: Union[Tuple, ArgumentProperties], flag_index: int = 0) -> str:
+        if isinstance(argument_properties, ArgumentProperties):
+            flags = argument_properties.flags
+        else:
+            flags = argument_properties[3]
+        return flags[flag_index]
 
     def _get_parser_argument(self):
         return getattr(self, '__parser_argument')
