79e10e9e0eba7b9244a9795a050a0a6e6776ecae
Priit Lätt <lattpriit@gmail.com>
2023-01-18 18:11:47 +0200

    Take pre-release and App Store versions into account when looking for latest build number

--- tests/tools/app_store_connect/test_get_latest_build_number.py
+++ tests/tools/app_store_connect/test_get_latest_build_number.py
@@ -1,38 +1,58 @@
 import dataclasses
 import uuid
+from typing import Tuple
+from typing import Type
+from typing import TypeVar
 from unittest import mock
 
 import pytest
 
 from codemagic.apple.resources import Build
+from codemagic.apple.resources import PreReleaseVersion
+from codemagic.apple.resources import Resource
 from codemagic.apple.resources import ResourceType
 from codemagic.tools.app_store_connect import AppStoreConnect
+
+R = TypeVar('R', bound=Resource)
 
 
 def _mock_echo(*_args, **_kwargs):
     pass
 
 
-def _get_build(version) -> Build:
-    attributes = {f.name: None for f in dataclasses.fields(Build.Attributes)}
-    attributes['version'] = version
-    return Build({
-        'attributes': attributes,
+def _get_resource(version: str, resource_type: ResourceType, resource_class: Type[R]) -> R:
+    return resource_class({
+        'attributes': {
+            **{f.name: None for f in dataclasses.fields(resource_class.Attributes)},
+            'version': version,
+        },
         'id': str(uuid.uuid4()),
         'links': {'self': None},
-        'type': ResourceType.BUILDS.value,
+        'type': resource_type.value,
     })
 
 
-@pytest.mark.parametrize('versions, expected_latest_build_number', [
-    ([], None),
-    (['1.0.1', '2', '0.1'], '2'),
-    (['2.0', '2'], '2.0'),
-    (['11', '13', '14', '15', '12'], '15'),
-    (['13.0.1.2.3.4.5'], '13.0.1.2.3.4.5'),
-])
+def _get_version_and_build(versions: Tuple[str, str]) -> Tuple[PreReleaseVersion, Build]:
+    version_name, build_number = versions
+
+    pre_release_version = _get_resource(version_name, ResourceType.PRE_RELEASE_VERSIONS, PreReleaseVersion)
+    build = _get_resource(build_number, ResourceType.BUILDS, Build)
+    return pre_release_version, build
+
+
+@pytest.mark.parametrize(
+    'version_pairs, expected_latest_build_number', [
+        (iter([]), None),
+        ([('1.0', '12345')], '12345'),
+        ([('1.2.3', '1.0.1'), ('1.2.3', '2'), ('1.2.3', '0.1')], '2'),
+        ([('1.2.3', '2.0'), ('1.2.3', '2')], '2.0'),
+        ([('1.0', '11'), ('1.0', '13'), ('1.0', '14'), ('1.0', '15'), ('1.0', '12')], '15'),
+        ([('3.0', '13.0.1.2.3.4.5')], '13.0.1.2.3.4.5'),
+        ([('3.0.9', '1'), ('3.0.9', '2'), ('3.0.8', '3')], '2'),
+    ],
+)
 @mock.patch.object(AppStoreConnect, 'echo', _mock_echo)
-def test_get_latest_build_number(versions, expected_latest_build_number):
-    builds = list(map(_get_build, versions))
-    latest_build_number = AppStoreConnect._get_latest_build_number(builds)
+def test_get_latest_build_number(version_pairs, expected_latest_build_number):
+    versions_and_builds = map(_get_version_and_build, version_pairs)
+    latest_build_number = AppStoreConnect._get_latest_build_number(versions_and_builds)
     assert latest_build_number == expected_latest_build_number
