fba675b195a6e3243f6365b332f978cdaf933111
Priit Lätt <lattpriit@gmail.com>
2023-12-18 17:53:49 +0200

    Define missing attributes and relationships for builds and apps

--- src/codemagic/apple/resources/app.py
+++ src/codemagic/apple/resources/app.py
@@ -5,6 +5,7 @@ from typing import Optional
 
 from .enums import ContentRightsDeclaration
 from .enums import Locale
+from .enums import SubscriptionStatusUrlVersion
 from .resource import Relationship
 from .resource import Resource
 
@@ -27,15 +28,46 @@ class App(Resource):
         contentRightsDeclaration: ContentRightsDeclaration
         isOrEverWasMadeForKids: bool
 
+        subscriptionStatusUrl: Optional[str] = None
+        subscriptionStatusUrlForSandbox: Optional[str] = None
+        subscriptionStatusUrlVersion: Optional[SubscriptionStatusUrlVersion] = None
+        subscriptionStatusUrlVersionForSandbox: Optional[SubscriptionStatusUrlVersion] = None
+
         def __post_init__(self):
             if isinstance(self.contentRightsDeclaration, str):
                 self.contentRightsDeclaration = ContentRightsDeclaration(self.contentRightsDeclaration)
             if isinstance(self.primaryLocale, str):
                 self.primaryLocale = Locale(self.primaryLocale)
+            if isinstance(self.subscriptionStatusUrlVersion, str):
+                self.subscriptionStatusUrlVersion = SubscriptionStatusUrlVersion(self.subscriptionStatusUrlVersion)
+            if isinstance(self.subscriptionStatusUrlVersionForSandbox, str):
+                self.subscriptionStatusUrlVersionForSandbox = SubscriptionStatusUrlVersion(
+                    self.subscriptionStatusUrlVersionForSandbox,
+                )
 
     @dataclass
     class Relationships(Resource.Relationships):
-        _OMIT_IF_NONE_KEYS = ("betaTesters", "ciProduct", "perfPowerMetrics")
+        _OMIT_IF_NONE_KEYS = (
+            "appAvailability",
+            "appAvailabilityV2",
+            "appClips",
+            "appCustomProductPages",
+            "appEvents",
+            "appPricePoints",
+            "appPriceSchedule",
+            "appStoreVersionExperimentsV2",
+            "betaTesters",
+            "ciProduct",
+            "customerReviews",
+            "gameCenterDetail",
+            "inAppPurchasesV2",
+            "perfPowerMetrics",
+            "pricePoints",
+            "promotedPurchases",
+            "reviewSubmissions",
+            "subscriptionGracePeriod",
+            "subscriptionGroups",
+        )
 
         appInfos: Relationship
         appStoreVersions: Relationship
@@ -52,6 +84,22 @@ class App(Resource):
         preReleaseVersions: Relationship
         prices: Relationship
 
+        appAvailability: Optional[Relationship] = None
+        appAvailabilityV2: Optional[Relationship] = None
+        appClips: Optional[Relationship] = None
+        appCustomProductPages: Optional[Relationship] = None
+        appEvents: Optional[Relationship] = None
+        appPricePoints: Optional[Relationship] = None
+        appPriceSchedule: Optional[Relationship] = None
+        appStoreVersionExperimentsV2: Optional[Relationship] = None
         betaTesters: Optional[Relationship] = None
         ciProduct: Optional[Relationship] = None
+        customerReviews: Optional[Relationship] = None
+        gameCenterDetail: Optional[Relationship] = None
+        inAppPurchasesV2: Optional[Relationship] = None
         perfPowerMetrics: Optional[Relationship] = None
+        pricePoints: Optional[Relationship] = None
+        promotedPurchases: Optional[Relationship] = None
+        reviewSubmissions: Optional[Relationship] = None
+        subscriptionGracePeriod: Optional[Relationship] = None
+        subscriptionGroups: Optional[Relationship] = None
