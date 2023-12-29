d72da4c6e9b95e19ac73593424b1ed79d34184fa
Priit Lätt <lattpriit@gmail.com>
2023-12-29 11:10:08 +0200

    Revert protocol usage

--- src/codemagic/cli/argument/action_callable.py
+++ src/codemagic/cli/argument/action_callable.py
@@ -2,22 +2,19 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-
-from typing_extensions import Protocol
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import Optional
+from typing import Sequence
 
 if TYPE_CHECKING:
-    from typing import Any
-    from typing import Callable
-    from typing import Dict
-    from typing import Optional
-    from typing import Sequence
-
     from ..action import DeprecationActionInfo
     from ..action_group import ActionGroup
     from .argument import Argument
 
 
-class ActionCallable(Protocol):
+class ActionCallable:
     action_group: Optional[ActionGroup]
     action_name: str
     arguments: Sequence[Argument]
