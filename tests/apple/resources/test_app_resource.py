806446be8023f20b4e28ed39f2e521f44ee950b0
Priit Lätt <lattpriit@gmail.com>
2024-04-24 11:08:25 +0300

    Update app resource response mock

--- tests/apple/resources/test_app_resource.py
+++ tests/apple/resources/test_app_resource.py
@@ -3,11 +3,6 @@ from __future__ import annotations
 from codemagic.apple.resources import App
 
 
-def test_app_initialization(api_app):
-    app = App(api_app)
-    assert app.dict() == api_app
-
-
 def test_app_with_empty_relationship(api_app):
     api_app["relationships"]["ciProduct"] = {}
     app = App(api_app)
