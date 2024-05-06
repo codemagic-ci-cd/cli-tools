61315a526f175c45a27105d6d4dac34c94663b84
Priit Lätt <lattpriit@gmail.com>
2024-05-06 12:51:24 +0300

    Reorder if-elif-else blocks

--- src/codemagic/mixins/path_finder.py
+++ src/codemagic/mixins/path_finder.py
@@ -10,17 +10,17 @@ class PathFinderMixin(metaclass=abc.ABCMeta):
     logger: logging.Logger
 
     def glob(self, pattern: pathlib.Path) -> Iterator[pathlib.Path]:
-        if pattern == pathlib.Path("."):
+        if pattern.is_absolute():
+            self.logger.info(f"Searching for files matching {pattern}")
+            # absolute globs are not supported, match them as relative to root
+            relative_pattern = pattern.relative_to(pattern.anchor)
+            return pathlib.Path(pattern.anchor).glob(str(relative_pattern))
+        elif pattern == pathlib.Path("."):
             self.logger.info(f"Searching for files matching {pattern.resolve()}")
             # `Path('.').glob('.')` which this would essentially cause is erroneous.
             # Return empty iterator as we cannot possibly find current directory from
             # within the same directory.
             return iter([])
-        elif pattern.is_absolute():
-            self.logger.info(f"Searching for files matching {pattern}")
-            # absolute globs are not supported, match them as relative to root
-            relative_pattern = pattern.relative_to(pattern.anchor)
-            return pathlib.Path(pattern.anchor).glob(str(relative_pattern))
         else:
             self.logger.info(f"Searching for files matching {pattern.resolve()}")
             return pathlib.Path().glob(str(pattern))
