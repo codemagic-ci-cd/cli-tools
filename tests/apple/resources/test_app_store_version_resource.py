4b09fe6ae61b261da0726368c6f73193be8d5fca
Priit Lätt <lattpriit@gmail.com>
2024-05-27 10:55:21 +0300

    Add test to replicate issue

--- tests/apple/resources/test_app_store_version_resource.py
+++ tests/apple/resources/test_app_store_version_resource.py
@@ -3,6 +3,13 @@ from __future__ import annotations
 from codemagic.apple.resources import AppStoreVersion
 
 
-def test_build_initialization(api_app_store_version):
+def test_app_store_version_initialization(api_app_store_version):
+    app_store_version = AppStoreVersion(api_app_store_version)
+    assert app_store_version.dict() == api_app_store_version
+
+
+def test_app_store_version_without_idfa_declaration(api_app_store_version):
+    api_relationships = api_app_store_version["relationships"]
+    del api_relationships["idfaDeclaration"]
     app_store_version = AppStoreVersion(api_app_store_version)
     assert app_store_version.dict() == api_app_store_version
