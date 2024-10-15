1b34b890a107c5558cfae67e7006f076dfe4eaff
Priit Lätt <lattpriit@gmail.com>
2024-10-15 16:35:40 +0300

    Fix test

--- tests/models/xctests/test_xcresulttool.py
+++ tests/models/xctests/test_xcresulttool.py
@@ -10,15 +10,15 @@ from packaging.version import Version
     ("xcresulttool_version", "expected_result"),
     (
         (None, False),
-        (Version("0"), False),
-        (Version("1"), False),
-        (Version("23020.9"), False),
-        (Version("23021"), True),
-        (Version("23021.1"), True),
-        (Version("23022"), True),
-        (Version("33022"), True),
+        (Version("0"), True),
+        (Version("1"), True),
+        (Version("23020.9"), True),
+        (Version("23021"), False),
+        (Version("23021.1"), False),
+        (Version("23022"), False),
+        (Version("33022"), False),
     ),
 )
-def test_requires_legacy_flag(xcresulttool_version: Optional[Version], expected_result: bool):
+def test_is_legacy(xcresulttool_version: Optional[Version], expected_result: bool):
     with mock.patch.object(XcResultTool, "get_tool_version", new=mock.MagicMock(return_value=xcresulttool_version)):
-        assert expected_result is XcResultTool._requires_legacy_flag()
+        assert expected_result is XcResultTool.is_legacy()
