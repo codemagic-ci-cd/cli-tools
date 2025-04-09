bd44e2f2fbfe388ae686571f9688472ad22de10c
Priit Lätt <lattpriit@gmail.com>
2025-04-09 15:30:51 +0300

    Fix converting bytes to string on Windows using Windows-1252 encoding

--- src/codemagic/mixins/str_converter.py
+++ src/codemagic/mixins/str_converter.py
@@ -1,3 +1,4 @@
+import os
 from typing import AnyStr
 from typing import Union
 
@@ -13,6 +14,15 @@ class StringConverterMixin:
     @classmethod
     def _str(cls, str_or_bytes: Union[str, bytes, AnyStr]) -> str:
         if isinstance(str_or_bytes, bytes):
-            return str_or_bytes.decode(encoding="utf-8")
+            return cls._bytes_to_str(str_or_bytes)
         else:
             return str_or_bytes
+
+    @classmethod
+    def _bytes_to_str(cls, bs: bytes) -> str:
+        try:
+            return bs.decode(encoding="utf-8")
+        except UnicodeDecodeError:
+            if os.name == "nt":  # Running on Windows, try Windows-1252 encoding too
+                return bs.decode(encoding="windows-1252")
+            raise
